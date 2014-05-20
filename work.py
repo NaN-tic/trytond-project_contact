3b300339fe876b3ff8c899776daa4054ffb7df08
Sergi Almacellas Abellana <sergi@koolpi.com>
2014-05-20 14:12:28 +0200

    Initial commit

---
+++ work.py
@@ -0,0 +1,42 @@
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
+from trytond.model import ModelSQL, fields
+from trytond.pool import Pool, PoolMeta
+from trytond.pyson import Eval
+
+__all__ = ['WorkParty', 'Work']
+__metaclass__ = PoolMeta
+
+
+class WorkParty(ModelSQL):
+    'Work'
+    __name__ = "project.work-party.party"
+
+    work = fields.Many2One('project.work', 'Work',
+        required=True, select=True)
+    party = fields.Many2One('party.party', 'Party', required=True, select=True)
+
+
+class Work:
+    'Work'
+    __name__ = "project.work"
+
+    allowed_contacts = fields.Function(fields.One2Many('party.party',
+            None, 'Allowed Contacts', on_change_with=['party']),
+        'on_change_with_allowed_contacts')
+    contacts = fields.Many2Many('project.work-party.party', 'work',
+        'party', 'Contacts',
+        domain=[
+            ('id', 'in', Eval('allowed_contacts', [])),
+            ],
+        depends=['allowed_contacts'])
+
+    def on_change_with_allowed_contacts(self, name=None):
+        pool = Pool()
+        Employee = pool.get('company.employee')
+        res = [e.party.id for e in Employee.search([])]
+        if not self.party:
+            return res
+
+        res.extend(r.to.id for r in self.party.relations)
+        return res
