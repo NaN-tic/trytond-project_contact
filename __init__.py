4aa02185d72fba46ed824411ff83ec5381e974c8
Juanjo Garcia <juanjo.garcia@nan-tic.com>
2019-03-19 12:32:49 +0100

    Allow users to configure if the want to receive notifications of their own changes and add did the last change in the e-mail.

--- __init__.py
+++ __init__.py
@@ -4,6 +4,7 @@ from trytond.pool import Pool
 from . import work
 from . import default_rule
 from . import summary_contacts
+from . import user
 
 def register():
     Pool.register(
@@ -12,4 +13,5 @@ def register():
         default_rule.ContactParty,
         default_rule.DefaultRule,
         summary_contacts.SummaryContacts,
+        user.User,
         module='project_contact', type_='model')
